fbe59fa249abde09beb6604de360c5a510061e57
Travis Fantina <tfantina@gmail.com>
2016-08-22 13:01:53 -0400

    Elasticsearch

--- config/elasticsearch.rb
+++ config/elasticsearch.rb
@@ -1,8 +1,6 @@
-if Rails.env == "Production"
-  url = 'https://zt2z51yq:23853yzdexnwjrs1@jasmine-66632.us-east-1.bonsai.io'
-    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
-else
+if Rails.env.production?
+  cliet = Elasticsearch::Client.new host: ENV['https://8fsolna9:37thfu92eckey5r4@ivy-3059605.us-east-1.bonsai.io']
+    else
   url = 'http://localhost:9200/'
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
