d8692edab6ddb44e4e452f3f2f31d79eedadf799
Travis Fantina <tfantina@gmail.com>
2016-08-22 13:05:28 -0400

    Moved elasticsearch initializer

--- config/elasticsearch.rb
+++
@@ -1,7 +0,0 @@
-if Rails.env.production?
-  cliet = Elasticsearch::Client.new host: ENV['https://8fsolna9:37thfu92eckey5r4@ivy-3059605.us-east-1.bonsai.io']
-    else
-  url = 'http://localhost:9200/'
-    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
-end
