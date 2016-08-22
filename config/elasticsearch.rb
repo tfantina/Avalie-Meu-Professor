1102a088d2a66d9a69d6114def677db19faed3a3
Travis Fantina <tfantina@gmail.com>
2016-08-22 11:56:37 -0400

    Added elasticsearch URL

--- config/elasticsearch.rb
+++ config/elasticsearch.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
-  url = 'https://1nhdf4bq:tf1l6ojt0i5v92s@myrtle-4777484.us-east-1.bonsai.io'
-    Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['BONSAI_URL']
+  url = 'http://ivy-3059605.us-east-1.bonsai.io'
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else
   url = 'http://localhost:9200/'
