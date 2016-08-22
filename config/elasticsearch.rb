6c1c87990bed3850a072c02a8f37376af5ffc7c3
Travis Fantina <tfantina@gmail.com>
2016-08-22 12:56:28 -0400

    Elasticsearch fix

--- config/elasticsearch.rb
+++ config/elasticsearch.rb
@@ -1,5 +1,5 @@
 if Rails.env == "Production"
-  url = 'http://ivy-3059605.us-east-1.bonsai.io'
+  url = 'https://zt2z51yq:23853yzdexnwjrs1@jasmine-66632.us-east-1.bonsai.io'
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else
