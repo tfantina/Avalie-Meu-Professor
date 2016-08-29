28ef0c227cfdca422b491edfb100ed8a843e536b
Travis Fantina <tfantina@gmail.com>
2016-08-29 13:31:13 -0600

    Bonsai URL fixed

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  url = 'dev-mess'
+  url = 'http://ivy-3059605.us-east-1.bonsai.io'
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else
