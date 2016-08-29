30694d5fb99966b70e22b8be2640f8041b0a7421
Travis Fantina <tfantina@gmail.com>
2016-08-29 11:26:07 -0600

    Changed bonsai URL in elasticsearch

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  url = 'https://ivy-3059605.us-east-1.bonsai.io'
+  url = 'https://8fsolna9:37thfu92eckey5r4@ivy-3059605.us-east-1.bonsai.io'
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
 else
