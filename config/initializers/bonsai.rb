9f8498adf48ac4981ea700b631f03bac163caaf9
Travis Fantina <tfantina@gmail.com>
2016-08-30 15:11:25 -0600

    Moving the initializer around

--- config/initializers/bonsai.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env == "production"
-  url = 'http://ivy-3059605.us-east-1.bonsai.io'
-    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
-else
-  url = 'http://localhost:9200/'
-    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
-end
