2c19203586dacd030927f34a0582a9a06af97b9b
Travis Fantina <tfantina@gmail.com>
2016-08-30 15:15:52 -0600

    Initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,5 @@
 if ENV['http://ivy-3059605.us-east-1.bonsai.io']
-  url = 'http://ivy-3059605.us-east-1.bonsai.io'
-    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['http://ivy-3059605.us-east-1.bonsai.io'], logs: true})
 else
   url = 'http://localhost:9200/'
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
