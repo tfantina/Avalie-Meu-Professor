61e2b444c082609c891895be820d6b3914e72f00
Travis Fantina <tfantina@gmail.com>
2016-08-30 16:06:08 -0600

    Initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,10 @@
 require 'elasticsearch/model'
 
-if ENV['http://ivy-3059605.us-east-1.bonsai.io']
-  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['http://ivy-3059605.us-east-1.bonsai.io'], logs: true})
-end
+BONSAI_URL = ENV['BONSAI_URL'] ||
+  Rails.application.config_for(:elasticsearch)["http://ivy-3059605.us-east-1.bonsai.io"]
+
+# Set up an app-wide client object
+Elasticsearch::Model.client = Elasticsearch::Client.new(
+  url: BONSAI_URL,
+  log: true
+)
