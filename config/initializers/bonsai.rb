e5f19d7dcd89d8601956c9205374b514e363e92f
Travis Fantina <tfantina@gmail.com>
2016-09-12 14:34:28 -0600

    Bonsai

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,7 @@
 BONSAI_URL = ENV['beech-6556653.us-east-1.bonsai.io']
 
 if BONSAI_URL
-  uri = URI.parse(ENV['BONSAI_URL'])
+  uri = URI.parse(ENV['beech-6556653.us-east-1.bonsai.io'])
 Searchkick.client = Elasticsearch::Client.new({
   url:BONSAI_URL,
   log: true
