d4ce44504f146f02517ed55cd4b3197126e7a649
Travis Fantina <tfantina@gmail.com>
2016-09-12 13:35:41 -0600

    Heroku

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,9 @@
 BONSAI_URL = ENV['beech-6556653.us-east-1.bonsai.io']
 
 if BONSAI_URL
-  Elasticsearch::Model.client = Elasticsearch::Client.new(
-    url: BONSAI_URL,
-    log: true
-)
+#  Elasticsearch::Model.client = Elasticsearch::Client.new(    url: BONSAI_URL,  log: true)
+Searchkick.client = Elasticsearch::Client.new({
+  url:BONSAI_URL,
+  log: true
+  })
 end
