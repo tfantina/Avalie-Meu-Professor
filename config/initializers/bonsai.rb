4f75bcf144dd2a8eb3507247d28c3d7c9b1d2567
Travis Fantina <tfantina@gmail.com>
2016-10-21 16:53:16 -0600

    Bonsai please

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,7 +1,10 @@
 BONSAI_URL = ENV['privet-7322854.us-east-1.bonsai.io']
-#if BONSAI_URL
-Searchkick.client = Elasticsearch::Client.new({
+
+
+if BONSAI_URL
+Searchkick.client = Elasticsearch::Client.new(
   url:BONSAI_URL,
   log: true
-  })
-#end
+  )
+end
+#removed curley braces between prenthecises 
